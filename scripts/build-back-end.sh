c778c0e827ce1990af759602cb93e3ed7f949e78
蓝明乐 <lanmingle@qq.com>
2018-05-14 00:58:32 +0800

    update

--- scripts/build-back-end.sh
+++ scripts/build-back-end.sh
@@ -5,6 +5,12 @@ set -e
 cp -R /opt/src/rap2-easy-end/config/rap2-delos/*.ts \
 /opt/src/rap2-easy-end/source/rap2-delos/src/config/
 
+cp -R /opt/src/rap2-easy-end/bugs/rap2-delos/service/*.ts \
+/opt/src/rap2-easy-end/source/rap2-delos/src/service/
+
+cp -R /opt/src/rap2-easy-end/features/rap2-delos/routes/*.ts \
+/opt/src/rap2-easy-end/source/rap2-delos/src/routes/
+
 docker run --rm -it \
 -v="/opt/src/rap2-easy-end/config/npm/setting.npmrc:/root/.npmrc" \
 -v="/opt/src/rap2-easy-end/source/rap2-delos/:/opt/src/rap2-delos/" \
